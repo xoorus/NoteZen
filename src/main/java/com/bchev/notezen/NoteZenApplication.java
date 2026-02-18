4dbb52c82c77465cfcc23815ab0c9899868b1523
bastien <bchevriaut@gmail.com>
2026-02-18 13:43:58 +0100

    first commit

---
+++ src/main/java/com/bchev/notezen/NoteZenApplication.java
@@ -0,0 +1,13 @@
+package com.bchev.notezen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteZenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteZenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bchev/notezen/NoteZenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bchev.notezen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteZenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
